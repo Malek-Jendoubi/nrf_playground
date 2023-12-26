59945f624d086042cb995bb45efa6b6d5557bb85
Malek Jendoubi <malek.jendoubi@uni-rostock.de>
2023-12-26 12:08:01 +0100

    ADDED LOGGING via segger rtt

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+/*Include LOG Library*/
+#include <zephyr/logging/log.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -18,6 +20,9 @@
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+/*Register LOG Console*/
+LOG_MODULE_REGISTER(Less4_Exer2, LOG_LEVEL_DBG);
 
 int main(void)
 {
@@ -34,6 +39,8 @@ int main(void)
 
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
+		
+		LOG_DBG("A log message in debug level");
 		if (ret < 0) {
 			return 0;
 		}
